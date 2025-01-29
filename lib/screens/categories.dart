bbdb60112fd47200a0d42bd084f213a15f5b5dd6
Jacob Bunn <jarbunn18@gmail.com>
2025-01-29 03:16:27 -0700

    Added tab bar

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -23,11 +23,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Select a Category"),
-      ),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
@@ -43,7 +39,6 @@ class CategoriesScreen extends StatelessWidget {
               },
             ),
         ],
-      ),
     );
   }
 }
