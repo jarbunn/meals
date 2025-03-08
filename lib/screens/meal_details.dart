f2b5e62f188d313733a66e4f60836134191d6874
jarbunn18@gmail.com <jacob@pc-74.home>
2025-03-08 10:14:20 +0100

    Added favorite and meal image slide animations

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -15,28 +15,43 @@ class MealDetailsScreen extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(title: Text(meal.title), actions: [
         IconButton(
-            onPressed: () {
-              final wasAdded = ref
-                  .read(favoriteMealsProvider.notifier)
-                  .toggleMealFavoriteStatus(meal);
-              ScaffoldMessenger.of(context).clearSnackBars();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content:
-                      Text(wasAdded ? 'Meal Favorited' : 'Meal Unfavorited'),
-                ),
+          onPressed: () {
+            final wasAdded = ref
+                .read(favoriteMealsProvider.notifier)
+                .toggleMealFavoriteStatus(meal);
+            ScaffoldMessenger.of(context).clearSnackBars();
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(wasAdded ? 'Meal Favorited' : 'Meal Unfavorited'),
+              ),
+            );
+          },
+          icon: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            transitionBuilder: (child, animation) {
+              return RotationTransition(
+                turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                child: child,
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border)),
+            child: Icon(
+              isFavorite ? Icons.star : Icons.star_border,
+              key: ValueKey(isFavorite),
+            ),
+          ),
+        ),
       ]),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             SizedBox(
               height: 14,
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,11 +10,13 @@ class MealItem extends StatelessWidget {
   final void Function(Meal meal) onSelectMeal;
 
   String get complexityText {
-    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
   }
 
   String get affordabilityText {
-    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
   }
 
   @override
@@ -30,12 +32,15 @@ class MealItem extends StatelessWidget {
         },
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
@@ -62,11 +67,18 @@ class MealItem extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        MealItemTrait(icon: Icons.schedule, label: '${meal.duration} min'),
-                        const SizedBox(width: 12,),
+                        MealItemTrait(
+                            icon: Icons.schedule,
+                            label: '${meal.duration} min'),
+                        const SizedBox(
+                          width: 12,
+                        ),
                         MealItemTrait(icon: Icons.work, label: complexityText),
-                        const SizedBox(width: 12,),
-                        MealItemTrait(icon: Icons.attach_money, label: affordabilityText)
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                            icon: Icons.attach_money, label: affordabilityText)
                       ],
                     )
                   ],
